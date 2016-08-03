20f904c4ff774db33ec665ac980d59d884599eae
Ray Nicholus <github.com@garstasio.com>
2016-08-03 00:24:45 -0500

    chore(build): cleanup test-related Grunt deps #1569

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,11 @@ module.exports = function(grunt) {
 
     require("time-grunt")(grunt);
 
-    var allBrowsers, async, browsers, configs, fineUploaderModules, fs, name, path, paths, pkg, spawn, tasks, utils, uuid;
+    var async, configs, fs, name, path, paths, pkg, tasks, utils, uuid;
 
     fs = require("fs");
     uuid = require("uuid");
     async = require("async");
-    path = require("path");
-    spawn = require("child_process").spawn;
     utils = require("./lib/grunt/utils");
     configs = require("./lib/grunt/configs");
     tasks = "./lib/grunt/tasks";
@@ -29,12 +27,8 @@ module.exports = function(grunt) {
         build: "./_build",
         src: "./client",
         html: "./client/html/templates",
-        docs: "./docs",
-        test: "./test"
+        docs: "./docs"
     };
-    allBrowsers = require("./lib/browsers");
-    browsers = allBrowsers.browsers;
-    fineUploaderModules = require("./lib/modules");
     grunt.initConfig({
         pkg: pkg,
         paths: paths,
@@ -42,18 +36,13 @@ module.exports = function(grunt) {
         concat: configs.concat(paths),
         copy: configs.copy(paths),
         cssmin: configs.cssmin(paths),
-        nodestatic: configs["static"](paths),
         aws_s3: configs.s3(paths.dist, paths.build, pkg.version),
         shell: configs.shell(paths),
         strip_code: configs.stripcode(paths),
         uglify: configs.uglify(paths),
         usebanner: configs.banner(paths),
         version: configs.version(pkg),
-        watch: configs.watch(paths),
-        tests: {
-            local: "./lib/karma/karma-local.conf.js",
-            travis: "./lib/karma/karma-travis.conf.js"
-        }
+        watch: configs.watch(paths)
     });
 
     for (name in pkg.devDependencies) {
